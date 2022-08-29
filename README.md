8cc25a14bd30f2ae24d57f5b3f106de01a9656dd
kdjordan <k.dean.jordan@gmail.com>
2022-08-28 22:04:47 -0700

    readme added

---
+++ README.md
@@ -0,0 +1,13 @@
+## React Jobly Project
+
+#### STACK  
+- React (FE) => deployed using [Surge](https://create-react-app.dev/docs/deployment/)
+- Node / Express => deployed on Heroku
+- Postgres => included in Heroku deployment
+
+#### LIVE 
+Click here : [JOBLY](https://automatic-tub.surge.sh/)
+
+#### TEST USER CREDENTIALS
+- username: testuser
+- password: password
--- jobly-client/src/Api.js
+++ jobly-client/src/Api.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+require('dotenv').config()
 
 const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
 
@@ -14,7 +15,7 @@ class Jobly {
   static token 
   
   static async request(endpoint, data = {}, method = "get") {
-    console.debug("API Call:", endpoint, data, method);
+
 
     //there are multiple ways to pass an authorization token, this is how you pass it in the header.
     //this has been provided to show you another way to pass the token. you are only expected to read this code for this project.
